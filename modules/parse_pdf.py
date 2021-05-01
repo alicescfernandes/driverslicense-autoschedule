8bae542845bfdc6c17a6f684a7d818295610dace
Alice Fernandes <alice.fernandes@sky.uk>
2021-05-01 23:58:41 +0100

    parse PDF & scrap facebook page

--- modules/parse_pdf.py
+++ modules/parse_pdf.py
@@ -1,8 +1,33 @@
 import tabula
+import numpy as np
+from dotenv import dotenv_values
 
-df = tabula.read_pdf('cenas.pdf')[0]
+from tabula.io import read_pdf
+config = dotenv_values(".env")
 
-df.columns = df.columns.str.replace('\r', '')
-print(df[10:11])
-data = df[10:11]
-data.to_excel('data.xlsx')
+def parse_pdf():
+    aulas = {}
+    df = read_pdf("./assets/horario.pdf", pages=1)[0]
+
+    # Clean the freaking PDF
+    df.replace(None,False, regex="M.+", inplace=True)
+    df.replace(np.nan,False, inplace=True)
+    df.replace(None,"", regex="\D+", inplace=True)
+    df.replace('0',False, inplace=True)
+    
+    # Extract only the classes from 19H
+    data = df[10:11]
+
+    # Iterate through the columns, and create a dict with the days & classes
+    colunas = data.columns
+    for c in colunas[1::]:
+        aula = data[c].iloc[0]
+        if(aula):
+            aulas[int(aula)] = int(c)
+    return aulas
+    
+    # Export to excel, to validate manually
+    data.to_excel('./assets/horario.xlsx')
+
+if __name__ == "__main__":
+    parse_pdf()
---
+++ modules/scrapper.py
@@ -0,0 +1,40 @@
+import requests
+from dateutil.parser import parse
+from dotenv import dotenv_values
+from datetime import datetime
+
+meses = ["Janeiro", "Fevereiro", "Março","Abril","Maio","Junho","Julho","Agosto","Setempro"]
+config = dotenv_values(".env")
+
+def download_pdf():
+    success = False
+    url = config["HORARIO_PDF"]
+    r = requests.get(url)
+    date_modified = None
+    if (r.status_code == 200):
+        with open('./assets/horario.pdf', 'wb') as f:
+            f.write(r.content)
+            date_modified = parse(r.headers['Last-Modified'])
+            success = True
+    return (success, date_modified)
+
+
+def check_facebook():
+    success = False
+    next_month_online = False
+    url = config["FACEBOOK_URL"]
+    r = requests.get(url)
+    if (r.status_code == 200):
+        success = True
+        today = datetime.today()
+        current_day = today.day
+        if(current_day >= 27):
+            mes = meses[today.month]
+            next_month_online = '{0} de 2021'.format(mes) in str(r.content)
+
+    return (success,next_month_online)  
+      
+
+if __name__ == "__main__":
+    print(download_pdf())
+    print(check_facebook())
