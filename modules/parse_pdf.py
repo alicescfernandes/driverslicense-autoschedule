c8a94847fbb2c502740a96f70aa7b1802bcf55b8
Alice Fernandes <alice.fernandes@sky.uk>
2021-05-02 00:32:29 +0100

    test with different schedules format

--- modules/parse_pdf.py
+++ modules/parse_pdf.py
@@ -7,16 +7,24 @@ config = dotenv_values(".env")
 
 def parse_pdf():
     aulas = {}
-    df = read_pdf("./assets/horario.pdf", pages=1)[0]
+    df = None
+    dataframes = read_pdf("./assets/horario_2.pdf", pages=1)
 
+    # read_pdf returns multiple dataframes, we should only use the one with proper rows and columns
+    for d in dataframes:
+        if(d.empty is not True and 'DIA' in d.columns):
+            df = d
+    
+  
     # Clean the freaking PDF
     df.replace(None,False, regex="M.+", inplace=True)
     df.replace(np.nan,False, inplace=True)
     df.replace(None,"", regex="\D+", inplace=True)
     df.replace('0',False, inplace=True)
-    
-    # Extract only the classes from 19H
-    data = df[10:11]
+
+   
+    # Extract only the classes from 19H    
+    data = df.query("DIA == '19'")
 
     # Iterate through the columns, and create a dict with the days & classes
     colunas = data.columns
@@ -24,10 +32,13 @@ def parse_pdf():
         aula = data[c].iloc[0]
         if(aula):
             aulas[int(aula)] = int(c)
-    return aulas
-    
+
     # Export to excel, to validate manually
     data.to_excel('./assets/horario.xlsx')
+
+    return aulas
+
+   
 
 if __name__ == "__main__":
     parse_pdf()
