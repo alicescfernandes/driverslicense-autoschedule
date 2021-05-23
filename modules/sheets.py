68febfb235b330beb3af68ec11d49824e04f40c6
Alice Fernandes <alice.fernandes@sky.uk>
2021-05-23 14:34:08 +0100

    add restrições tempo

--- modules/sheets.py
+++ modules/sheets.py
@@ -1,17 +1,11 @@
 import gspread
 from dotenv import dotenv_values
-"""
-gc = gspread.service_account("./credentials.json")
-
-# Open a sheet from a spreadsheet in one go
-# Update a range of cells using the top left corner address
-"""
-
-
 
 # TODO: Marcar as restantes aulas por dia
 
 config = dotenv_values("./.env")
+
+# Restrições para as aulas sequenciais
 aulas_dependencias = {
     2:1,
     3:2,
@@ -25,18 +19,48 @@ aulas_dependencias = {
     28:27,
 }
 
+# Restrições para as aulas que precisem de horas praticas
+aulas_praticas_dependencias = {
+    24:16,
+    25:16,
+    26:16,
+    26:16,
+    28:16
+}
+
 class Database:
     gc = None
     aulas = []
 
     def __init__(self):
         self.gc = gspread.service_account("./credentials.json")
-
-    def get_worksheet(self, name="aulas"):
+    
+    # Retorna uma google sheet
+    def get_worksheet(self, name="teoricas"):
         sheets = self.gc.open_by_key(config["SHEET_NAME"])
         return sheets.worksheet(name)
+    
+    # Retorna todas as aulas práticas marcadas
+    def get_praticas(self):
+        sh = self.get_worksheet(name="praticas")
+        res = []
+        try:
+            [res] = sh.get("A2:A33",major_dimension="COLUMNS")
+        except Exception as e:
+            ''' '''
+        return  res
 
+
+    # Retorna true se aula pode ser marcada, false se aula não pode ser marcada
     def verificar_deps(self,aula):
+        # Verificar horas práticas
+        if(aula in aulas_praticas_dependencias):
+            horas = aulas_praticas_dependencias[aula]
+            praticas_marcadas = len(self.get_praticas())
+            if(horas > praticas_marcadas):
+                return False
+
+        # Verificar aulas sequenciais
         if(aula in aulas_dependencias):
             dependencia = aulas_dependencias[aula]
             (_,existe) = self.procurar_aula(dependencia)
@@ -54,7 +78,6 @@ class Database:
        
         try:
             sh.append_row([numero,marcacao],value_input_option="USER_ENTERED",insert_data_option='INSERT_ROWS')
-            sh.sort((1,'asc'))
         except Exception as e:
             print(e)
             success = False
@@ -71,7 +94,7 @@ class Database:
         result = []
         sh = self.get_worksheet()
         try:
-            [result] = sh.get("A1:A99",major_dimension="COLUMNS")
+            [result] = sh.get("A2:A99",major_dimension="COLUMNS")
             self.aulas = result
         except Exception as e:
             ""
@@ -92,7 +115,7 @@ class Database:
     def upsert_atualizar(self, data):
         success = False
         exists = False
-        sh = self.get_worksheet(name="update")
+        sh = self.get_worksheet(name="config")
 
         try:
             sh.update_cell(2,1,data)
@@ -104,7 +127,7 @@ class Database:
     def validar_data(self, data):
         success = False
         date = False
-        sh = self.get_worksheet(name="update")
+        sh = self.get_worksheet(name="config")
 
         try:
             [[date]] = sh.get("A2")
@@ -117,7 +140,7 @@ class Database:
     def get_total_marcacoes(self):
         success = False
         result = False
-        sh = self.get_worksheet(name="update")
+        sh = self.get_worksheet(name="config")
         
         try:
             [[result]] = sh.get("B2")
@@ -130,16 +153,21 @@ class Database:
 
 if __name__ == "__main__":
     db = Database()
-    #print(db.upsert_atualizar("2021-05-01"))
-    #print(db.adicionar_aula(23, "2021-05-22 14:00:00.00"))
-    #print(db.procurar_aula(23))
-    #db.adicionar_aula(24, "2021-05-22 14:00:00.00")
-    #print(db.procurar_aula(20))
-    #print(db.procurar_aula(1))
-    db.get_todas_aulas()
+    print(db.get_todas_aulas())
+    print(db.upsert_atualizar("2021-05-01"))
+    print(db.adicionar_aula(23, "2021-05-22 14:00:00.00"))
+    print(db.procurar_aula(23))
+    
+    print(db.adicionar_aula(24, "2021-05-22 14:00:00.00"))
+    print(db.procurar_aula(20))
+    print(db.procurar_aula(1))
+    
     print(db.procurar_aula(23))
     print(db.get_total_marcacoes())
-    #print(db.validar_data("2021-05-03"))
-    #print(db.verificar_deps(17))
-    #print(db.verificar_deps(2))
-    #print(db.verificar_deps(28))
+    print(db.get_praticas())
+    print(db.validar_data("2021-05-03"))
+   
+    print(db.verificar_deps(17))
+    print(db.verificar_deps(2))
+    print(db.verificar_deps(24))
+    print(db.verificar_deps(28))
